6291a02e5824978fb65bcb2f78b6922d1c6dda4f
Michaux Kelley <michauxkelley@gmail.com>
2018-08-12 21:51:31 -0400

    Add go back to block link

--- src/components/TransactionDetail/TransactionDetail.js
+++ src/components/TransactionDetail/TransactionDetail.js
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import createHistory from 'history/createBrowserHistory';
+import Link from '../Link';
 
 import s from '../Page/Page.css';
 import api from '../../api/client/api';
@@ -14,14 +17,19 @@ class TransactionDetail extends Component {
     this.state = {
       transaction: null,
     };
+    this.onClickGoBack = this.onClickGoBack.bind(this);
   }
   componentDidMount() {
+    this.history = createHistory();
     api
       .getByHash(this.props.hash, 'transactions')
       .then(res => this.setState({ transaction: res.data }))
       .catch(err => {
         throw Error(err);
       });
+  }
+  onClickGoBack() {
+    this.history.goBack();
   }
   static renderDetail(transaction) {
     if (!transaction) return <div>Loading...</div>;
@@ -78,6 +86,9 @@ class TransactionDetail extends Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
+          <Link to="#" onClick={this.onClickGoBack}>
+            Go Back to Block
+          </Link>
           <section>
             <h2 className={s.breakWord}>Transaction: {this.props.hash}</h2>
             {TransactionDetail.renderDetail(this.state.transaction)}
